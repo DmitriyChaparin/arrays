781611125bd0e0c50d3c025d7ef1c52b005bec3b
User <333@fd.com>
2022-10-10 21:41:09 +0400

    Решено

--- src/Main.java
+++ src/Main.java
@@ -13,24 +13,25 @@ public class Main {
         int[] total = {12, 15, 78, 33, 55, 0, 0};
 
         //Задача 2
-        System.out.print(numb[0]+ ", " + numb[1]+ ", "+ numb[2] + "\n");
+        System.out.print(numb[0] + ", " + numb[1] + ", " + numb[2] + "\n");
         System.out.print(numb1[0] + ", " + numb1[1] + ", " + numb1[2] + "\n");
         System.out.print(total[0] + ", " + total[1] + ", " + total[2] + ", " + total[3] + ", " + total[4] + ", " + total[5] + ", " + total[6]);
 
         //Задача 3
-        System.out.print(numb[2]+ ", " + numb[1]+ ", "+ numb[0] + "\n");
+        System.out.print(numb[2] + ", " + numb[1] + ", " + numb[0] + "\n");
         System.out.print(numb1[2] + ", " + numb1[1] + ", " + numb1[0] + "\n");
         System.out.print(total[6] + ", " + total[5] + ", " + total[4] + ", " + total[3] + ", " + total[2] + ", " + total[1] + ", " + total[0] + "\n");
 
         //Задача 4
-        for (int i = 0; i < numb.length ; i ++) {
-            if ( numb [i] %2 !=0 );{
-                numb [i] = numb[i] + 1;
-             System.out.print(numb[i] + ", ");}
-
+        for (int i = 0; i < numb.length; i++) {
+            if (numb[i] % 2 != 0) {
+                numb[i] = numb[i] + 1;
+                System.out.print(numb[i] + ", ");
+            } else {
+                System.out.print(numb[i] + ", ");
+            }
 
         }
-
 
     }
 }
